fdc64b0ec5e480e52518844da10ed2055a41e44a
Elisa <elisa.torazza@gmail.com>
2020-10-07 16:55:57 +0200

    Header

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Discs</title>
+    <link rel="stylesheet" href="dist/app.css">
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <img src="img/logo-spotify.png" alt="Logo Spotify">
+      </div>
+      
+    </header>
+
+  </body>
+</html>
